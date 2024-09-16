33b9fbefb4892ff425acc626a99e56e357b3848f
muha <Mohamed.Abdulle@metropolia.fi>
2024-09-16 14:29:57 +0300

    th valmis

---
+++ State/src/MasterState.java
@@ -0,0 +1,16 @@
+public class MasterState implements State {
+    @Override
+    public void train(GameCharacter character) {
+        System.out.println("You are a master now. No need to train.");
+    }
+
+    @Override
+    public void meditate(GameCharacter character) {
+        System.out.println("You are a master now. No need to meditate.");
+    }
+
+    @Override
+    public void fight(GameCharacter character) {
+        System.out.println("You are a master now. No need to fight.");
+    }
+}
