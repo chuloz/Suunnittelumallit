4dde67010a7146778c567c03668f9cce3dc7f464
muha <Mohamed.Abdulle@metropolia.fi>
2024-10-09 17:26:27 +0300

    valmis

---
+++ Strategy/src/ContextSort.java
@@ -0,0 +1,11 @@
+public class ContextSort {
+    private Strategy strategy;
+
+    public void setStrategy(Strategy strategy) {
+        this.strategy = strategy;
+    }
+
+    public void sort(int[] array) {
+        strategy.sort(array);
+    }
+}
---
+++ Strategy/src/Strategy.java
@@ -0,0 +1,4 @@
+
+public interface Strategy {
+    void sort(int[] array);
+}
